ce688d1fa80ad51c34ab01bfd77cb16752d63ea6
2bit-hack <sohamkar76@gmail.com>
2020-08-01 09:53:59 +0530

    Post eject commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const startApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  serviceWorker.unregister();
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if (window.cordova) {
+  document.addEventListener('#deviceready', startApp, false);
+} else {
+  startApp();
+}
+
