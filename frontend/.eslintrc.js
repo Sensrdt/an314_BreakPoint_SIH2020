b359b697af290014a6cd15bff0b1c64b0514fd59
Sridip <stsdutta2@gmail.com>
2020-08-01 13:08:10 +0530

    props validation error on lineChart.js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,21 +1,24 @@
 module.exports = {
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
     },
-  },
-  settings: {
-    react: {
-      version: 'detect',
+    settings: {
+        react: {
+            version: 'detect',
+        },
     },
-  },
-  env: {
-    browser: true,
-    jest: true,
-    node: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  plugins: ['react', 'import', 'jsx-a11y', 'react-hooks'],
+    env: {
+        browser: true,
+        jest: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    plugins: ['react', 'import', 'jsx-a11y', 'react-hooks'],
+    rules: {
+        'react/prop-types': 0,
+    },
 };
