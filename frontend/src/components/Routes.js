6152e08393a7f54df30fd72b2d7bf573e05afda8
2bit-hack <sohamkar76@gmail.com>
2020-08-02 12:42:46 +0530

    Add druglist and login screens

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Router, Switch, Route } from 'react-router-dom';
 
 import login from './JoinOption';
-import guidelines from './Menus/Guidelines/guide';
 import history from './history';
 import Menus from './Menus';
 import DrugList from './DrugList';
