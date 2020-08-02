aa7a39b84c28c6638b2d6059a26a9f9b479b2574
Sridip <stsdutta2@gmail.com>
2020-08-02 10:19:26 +0530

    Database removed

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,29 +1,32 @@
 module.exports = {
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
     },
-  },
-  settings: {
-    react: {
-      version: 'detect',
+    settings: {
+        react: {
+            version: 'detect',
+        },
     },
-  },
-  env: {
-    browser: true,
-    jest: true,
-    node: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  plugins: ['react', 'import', 'jsx-a11y', 'react-hooks', 'prettier'],
-  rules: {
-    'react/prop-types': 0,
-    'prettier/prettier': 1,
-    quotes: [2, 'single', 'avoid-escape'],
-    'import/no-useless-path-segments': [2, {
-      noUselessIndex: true,
-    }]
-  },
+    env: {
+        browser: true,
+        jest: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    plugins: ['react', 'import', 'jsx-a11y', 'react-hooks', 'prettier'],
+    rules: {
+        'react/prop-types': 0,
+        'prettier/prettier': 1,
+        quotes: [2, 'single', 'avoid-escape'],
+        'import/no-useless-path-segments': [
+            2,
+            {
+                noUselessIndex: true,
+            },
+        ],
+    },
 };
