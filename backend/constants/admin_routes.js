a9db3c7891af66e65395fea22b006e9f38316cc8
2bit-hack <sohamkar76@gmail.com>
2020-08-03 04:42:46 +0530

    Add administration routes

---
+++ backend/constants/admin_routes.js
@@ -0,0 +1,13 @@
+module.exports = [
+    'Convenience',
+    'Desired target effect',
+    'Oral',
+    'Local',
+    'Mouth inhalation',
+    'Nasal inhalation',
+    'Parenteral',
+    'Intranasal',
+    'Sublingual',
+    'Buccal',
+    'Sublabial',
+];
