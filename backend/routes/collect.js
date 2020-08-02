9eed41d4711a8684a6150dc8049aeb7806c0f1f7
2bit-hack <sohamkar76@gmail.com>
2020-08-03 02:53:33 +0530

    Add drug usage calculation logic

--- backend/routes/collect.js
+++ backend/routes/collect.js
@@ -2,18 +2,33 @@ const { Router } = require('express');
 const supl_post = require('../models/post/supl_post');
 const pres_post = require('../models/post/pres_post');
 const disp_post = require('../models/post/disp_post');
+const results = require('../models/results');
+const states = require('../constants/states_uts');
+const ages = require('../constants/age_groups');
 const router = new Router();
-
-// const calculateDrugActivity = (quantity, ddd) => {
-//     const drugActivity = quantity / ddd;
-//     return drugActivity;
-// };
 
 router.post('/supl', async (req, res) => {
     try {
         const data = req.body.payload;
         supl_post.insertOne(data, (err) => {
             console.log(err);
+        });
+
+        const {
+            dosage: { strength },
+            quantity: { amount, sub_amount },
+        } = data;
+        const drugActivity = amount * sub_amount * strength;
+        results.findOne({ drugName: data.drug }, (err, result) => {
+            if (err) {
+                res.status(404).send();
+            }
+            result.sourceDrugActivity.supplier += drugActivity;
+            result.save((err) => {
+                if (err) {
+                    res.status(404).send();
+                }
+            });
         });
     } catch (e) {
         res.status(500).send();
@@ -26,6 +41,31 @@ router.post('/pres', async (req, res) => {
         pres_post.insertOne(data, (err) => {
             console.log(err);
         });
+
+        const {
+            duration,
+            dosage: { strength },
+        } = data;
+        const drugActivity = duration * strength;
+        results.findOne({ drugName: data.drug }, (err, result) => {
+            if (err) {
+                res.status(404).send();
+            }
+            result.sourceDrugActivity.prescriber += drugActivity;
+            let stateIndex = states.indexOf(data.location);
+            if (stateIndex !== -1) {
+                result.stateDrugActivity[stateIndex] += drugActivity;
+            }
+            let ageIndex = ages.indexOf(data.ageGroup);
+            if (ageIndex !== -1) {
+                result.ageDrugActivity[ageIndex] += drugActivity;
+            }
+            result.save((err) => {
+                if (err) {
+                    res.status(404).send();
+                }
+            });
+        });
     } catch (e) {
         res.status(500).send();
     }
@@ -36,6 +76,27 @@ router.post('/disp', async (req, res) => {
         const data = req.body.payload;
         disp_post.insertOne(data, (err) => {
             console.log(err);
+        });
+
+        const {
+            dosage: { strength },
+            quantity: { amount, sub_amount },
+        } = data;
+        const drugActivity = amount * sub_amount * strength;
+        results.findOne({ drugName: data.drug }, (err, result) => {
+            if (err) {
+                res.status(404).send();
+            }
+            result.sourceDrugActivity.dispenser += drugActivity;
+            let stateIndex = states.indexOf(data.location);
+            if (stateIndex !== -1) {
+                result.stateDrugActivity[stateIndex] += drugActivity;
+            }
+            result.save((err) => {
+                if (err) {
+                    res.status(404).send();
+                }
+            });
         });
     } catch (e) {
         res.status(500).send();
