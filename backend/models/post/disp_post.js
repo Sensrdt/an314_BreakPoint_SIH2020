70b52cc327653f4aa9aa2d4420777d374e523d63
2bit-hack <sohamkar76@gmail.com>
2020-08-02 18:58:07 +0530

    Modified schemas

--- backend/models/post/disp_post.js
+++ backend/models/post/disp_post.js
@@ -3,6 +3,7 @@ const { Schema, model } = require('mongoose');
 const schema = new Schema({
     drug: {
         type: 'String',
+        required: true,
     },
     dosage: {
         form: {
@@ -11,15 +12,57 @@ const schema = new Schema({
         strength: {
             type: 'String',
         },
+        required: true,
     },
     quantity: {
         amount: 'Number',
         sub_amount: 'Number',
         scale: 'String',
+        required: true,
     },
     location: {
         type: 'String',
-    }
+        enum: [
+            'Andhra Pradesh',
+            'Arunachal Pradesh',
+            'Assam',
+            'Bihar',
+            'Chhattisgarh',
+            'Goa',
+            'Gujarat',
+            'Haryana',
+            'Himachal Pradesh',
+            'Jharkhand',
+            'Karnataka',
+            'Kerala',
+            'Madhya Pradesh',
+            'Maharashtra',
+            'Manipur',
+            'Meghalaya',
+            'Mizoram',
+            'Nagaland',
+            'Odisha',
+            'Punjab',
+            'Rajasthan',
+            'Sikkim',
+            'Tamil Nadu',
+            'Telangana',
+            'Tripura',
+            'Uttarakhand',
+            'Uttar Pradesh',
+            'West Bengal',
+            'Andaman and Nicobar Islands',
+            'Chandigarh',
+            'Dadra and Nagar Haveli',
+            'Daman & Diu',
+            'The Government of NCT of Delhi',
+            'Jammu & Kashmir',
+            'Ladakh',
+            'Lakshadweep',
+            'Puducherry',
+        ],
+        required: true,
+    },
 });
 
 const Data = model('data_disp', schema, 'data_disp');
