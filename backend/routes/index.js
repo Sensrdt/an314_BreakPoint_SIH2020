ad3ad0dc9b914db46e272cc87b2e5c2059d751ca
2bit-hack <sohamkar76@gmail.com>
2020-08-03 09:36:05 +0530

    Add userdata.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 const drug = require('./drug');
 const target = require('./target');
 const collect = require('./collect');
+const userdata = require('./userdata');
 
 const app = Router();
 
 app.use('/target', target);
 app.use('/drug', drug);
 app.use('/collect', collect);
+app.use('/user', userdata);
 
 module.exports = app;
---
+++ backend/routes/userdata.js
@@ -0,0 +1,27 @@
+const { Router } = require('express');
+const router = new Router();
+
+const User = require('../models/user');
+
+router.post('/', async (req, res) => {
+    try {
+        const { googleId } = req.user;
+        User.findOne({ googleId }, (err, user) => {
+            if (err) {
+                res.status(404).send();
+            }
+            const { location, userType } = req.body;
+            user.location = location;
+            user.userType = userType;
+            user.save((err) => {
+                if (err) {
+                    res.status(500).send();
+                }
+            });
+        });
+    } catch (err) {
+        res.status(500).send();
+    }
+});
+
+module.exports = router;
